e715b844f424a987354bb6e1e89885e32562a374
Artem Negel <artemnegel@gmail.com>
2022-02-05 12:58:50 +0200

    add about, carousel, footer sections

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    arrows: false,
+	dots: true,
+	autoplay: false,
+	autoplaySpeed: 2000,
+	slidesPerRow: 1,
+	slidesToShow: 3,
+	centerMode: true,
+	centerPadding: '0px',
+	responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        centerPadding: '90px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
+});
+	
